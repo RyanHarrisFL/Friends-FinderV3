e7a8a95e58cd91dd52f2b967eafbbce8d2514596
Rhino <ryanharrisfl@gmail.com>
2019-04-04 16:53:51 -0400

    changes to values

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,24 @@
+var path = require("path");
+
+module.exports = function(app) {
+  
+    app.get("/home", function(req, res) {
+      res.sendFile(path.join(__dirname, "./public/home.html"));
+    });
+  
+    app.get("/survey", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+  
+    // If no matching route is found default to home
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+  };
 
 
 
-/*
+  /*
 var array1 = [2, 4, 5, 6, 20, 8, 9, 10]
 
 var array2 = [5, 5, 13, 20, 8, 9, 20, 11]
@@ -35,24 +52,3 @@ if (execute === false) {
 difference(array1);
 
 */
-var path = require("path");
-
-module.exports = function(app) {
-    // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases the user is shown an HTML page of content
-    // ---------------------------------------------------------------------------
-  
-    app.get("/home", function(req, res) {
-      res.sendFile(path.join(__dirname, "./public/home.html"));
-    });
-  
-    app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
-  
-    // If no matching route is found default to home
-    app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-  };
