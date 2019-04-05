219ea780c2ccc1f3c9acdf62d76848d2e05e5c53
Rhino <ryanharrisfl@gmail.com>
2019-04-05 10:10:38 -0400

    changes to formatting

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,20 @@
-var friends = require("../data/friends");
-
-//var hotNewFriends = [];
+var possibleFriend = require("../data/friends");
 
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
-      res.json(friends);
+      res.json(possibleFriend);
     });
 
-  
+    app.post("/api/friends", function(req, res){
+
+     console.log(req)
+      console.log(req.body)
+      possibleFriend.push(req.body);
+      console.log(possibleFriend);
+    });
 
 }
-
- 
 
 var array1 = [2, 4, 5, 6, 20, 8, 9, 10];
 var array2 = [5, 5, 13, 20, 8, 9, 20, 11];
--- server.js
+++ server.js
@@ -15,8 +15,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 //Pointing to route files
-require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 
 
 //starting server, so it can listen for our app requests.
